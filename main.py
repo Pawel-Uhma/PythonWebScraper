c8d73d70a0ed3a5a8d4cc0f713b13b4743f46105
Pawel <paweluhma136@gmail.com>
2023-12-23 18:19:51 +0100

    Save offers to csv

--- main.py
+++ main.py
@@ -1,33 +1,90 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import csv
 from offer import Offer
 
+name_of_classes = {
+    "title"                 :   "css-16v5mdi er34gjf0",
+    "price"                 :   "css-10b0gli er34gjf0",
+    "location_date"         :   "css-veheph er34gjf0",
+    "size_price_per_meter"  :   "css-643j0o"
+}                           
 
-url = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/warszawa/"
-text_file = "text.txt"
 
-page = urlopen(url)
+def location_size_splitter(text_to_split:str)-> str:
+    part = text_to_split.split("-")
+    if len(part) <= 2:
+        return "","","",""
+    location = part[0]
+    price_per_meter = part[2]
+    date_n_size = part[1]
+    date, size = split_date_from_size(date_n_size)
+    return location, date, size, price_per_meter
 
-html = page.read()
-html = html.decode("utf-8")
+def split_date_from_size(text_to_split) ->str:
+    splitted = text_to_split.split()
+    if splitted[0] == "Dzisiaj":
+        date = splitted[0]+" "+ splitted[2][:5]
+        size= splitted[2][5:]
+        return date, size
+    
+    if len(splitted) >= 3:
+        date = splitted[0] + " " + splitted[1] + " " +splitted[2][:4]
+        size = splitted[2][4:]
+        return date,size
+    else: 
+        return "",""
+    
+def save_offers_to_csv(file_name, offers):
+    csv_file = open(file_name, 'w',newline = '',encoding="utf8")
+    writer = csv.writer(csv_file,delimiter = ',')
+    for offer in offers:
+        # offer.print()
+        writer.writerow(offer.get())
+    csv_file.close()
 
-soup = BeautifulSoup(html,"html.parser")
+def scrap_offers():
+    url = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/warszawa/"
+    file_name = "off.csv"
+    page = urlopen(url)
+    html = page.read()
+    html = html.decode("utf-8")
+    soup = BeautifulSoup(html,"html.parser")
 
-file = open(text_file, 'w',encoding="utf8")
-file.write(str(soup))
-file.close()
+    f = open("text.txt",'w',encoding="utf8")
+    f.write(str(soup))
+    f.close()
 
-offer_titles = soup.find_all("h6", {"class": "css-16v5mdi er34gjf0"})
+    offer_tags = soup.find_all("div", {"class": "css-1apmciz"})
+    offers = []
 
-number_of_offers = len(offer_titles)
-offers = []
+    for id_offer, offer_tag in enumerate(offer_tags):
+        title_tag = offer_tag.find_next().find_next()
+        title = title_tag.text
+        price_tag = title_tag.find_next()
+        price = price_tag.text
+        location_tag = price_tag.find_next().find_next()
+        location_n_size = location_tag.text
+        location, date, size, price_per_meter = location_size_splitter(location_n_size)
+        print(price + " - " + size + " - " + price_per_meter + " - " + date)
+        offer_instance = Offer(title, price,location, date, size, price_per_meter)
+        offers.append(offer_instance)
+    save_offers_to_csv(file_name,offers)
 
-for id_offer, offer_title_tag in enumerate(offer_titles):
-    offer_price = offer_title_tag.find_next().text
-    # print(offer_price)
-    offer_location = offer_title_tag.parent.find_next().find_next().text
-    print(offer_location)
-    #offer_instance = Offer(offer_title.text,offer_price)
-    #offers.append(offer_instance)
+def find_next_tag_by_class_name(tag, class_name):
+    current_tag = tag
+    while True:
+        if not current_tag.has_attr('class'):
+            current_tag = current_tag.find_next()
+        else:
+            if current_tag['class'][0] == class_name:
+                return current_tag
+            else:
+                current_tag = current_tag.find_next()
+    
+
+if __name__ == "__main__":
+    scrap_offers()
+
 
 
--- offer.py
+++ offer.py
@@ -1,13 +1,33 @@
 
 class Offer:
-    def __init__(self ,title ,price ,size ,location) -> None:
-        self.title = title
-        self.price = price
-        self.size = size
-        self.location = location
+    def __init__(self ,title, price ,location , date, size, price_per_meter):
         self.negotiable = None
-        return self
+        self.title = comma_to_dot(title)
+        # self.price = self.convert_price(comma_to_dot(price))
+        self.price = comma_to_dot(price)
+        self.location = remove_comma(location)
+        self.date=  comma_to_dot(date)
+        self.size = comma_to_dot(size)
+        self.price_per_meter = self.convert_price_per_meter(comma_to_dot(price_per_meter))
+    
     def print(self):
-        print(self.title + " - "+ self.price + " - "+self.size + " - "+self.location )
+        print(self.title + " - "+ self.price + " - "+self.location+ " - "+self.date + " - "+self.size+" - "+self.price_per_meter+ " - negotiable: "+ str(self.negotiable) )
+    def get(self):
+        return [self.title, self.price,self.location,self.date,self.size,self.price_per_meter,self.negotiable]
+    def convert_price(self,price:str):
+        price = price.replace('zł','')
+        if 'do negocjacji' in price:
+            self.negotiable = True
+            price = price.replace('do negocjacji','')
+        else:
+            self.negotiable = False
+        return price
+    def convert_price_per_meter(self,price_per_meter:str):
+        return price_per_meter[:-5]
 
-        
+def comma_to_dot(string:str):
+    return string.replace(',','.')
+def remove_comma(string:str):
+    return string.replace(',','')
+   
+
